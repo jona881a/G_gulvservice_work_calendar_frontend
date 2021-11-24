fec3680c882520719f4a7462a5ca8ce475665591
khade <khad0122@stud.kea.dk>
2021-11-24 09:21:56 +0100

    failed create assignment

---
+++ .idea/html/js/createAssignment.js
@@ -0,0 +1,63 @@
+
+document.addEventListener("DOMContentLoaded",createFormEvent);
+
+function createFormEvent(){
+    const formObject = document.getElementById("assign");
+    formObject.addEventListener("submit",handleAssignmentSubmit);
+}
+
+async function handleAssignmentSubmit(event){
+    console.log("start handleMovieSubmit");
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try{
+        const formData = new FormData(form);
+        await insertAssignmentInBackend(url,formData);
+        console.log("insertAssignmentInBackend running");
+
+    }catch(error){
+        console.log("Error in function handleAssignmentSubmit "+error.message)
+    }
+    window.location.href = "/G_gulvservice_work_calendar_frontend/html/assignment.html";
+}
+
+async function insertAssignmentInBackend(url,formData){
+
+    const plainFormData = Object.fromEntries(formData.entries());
+
+    let movieJSON = {
+
+        adress: plainFormData.adress,
+        assignmentStartTime :plainFormData.assignmentStartTime,
+        assignmentEndTime: plainFormData.assignmentEndTime,
+        assignmentStartDate:plainFormData.assignmentStartDate,
+        assignmentEndDate:plainFormData.assignmentEndDate,
+        color:plainFormData.color,
+        description:plainFormData.description,
+
+    }
+
+    const JSONObjectToJSONString = JSON.stringify(AssignmentJSON);
+
+    console.log(JSONObjectToJSONString);
+    console.log("url: "+url);
+
+    const POSTOptions = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSONObjectToJSONString
+    }
+
+
+    const response = await fetch(url,POSTOptions);
+    return response.json();
+
+
+
+}
+
