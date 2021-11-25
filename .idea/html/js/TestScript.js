efa9a909fde3a985d7466cde4fec5b662b629099
oliverbskau <oliverbskau@gmail.com>
2021-11-25 17:49:48 +0100

    første version af kalender færdig, bruger localStroage til at holde data i browser, mangler database manipulation

--- .idea/html/js/TestScript.js
+++ .idea/html/js/TestScript.js
@@ -4,16 +4,40 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 
 const calendar = document.getElementById('calendar');
-const weekdays = ['Sunday','Thuesday','Wednesday','Thursday','Friday','Saturday','Monday'];
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
+const weekdays = ['Monday','Thuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+function openModal(date){
+    clicked = date;
+
+    const eventForDay = events.find(e => e.date === clicked);
+
+    if (eventForDay){
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display = 'block';
+    }else{
+        newEventModal.style.display = 'block';
+    }
+
+    backDrop.style.display = 'block';
+}
+
 
 function load(){
     const dt = new Date();
 
-    const date = dt.getDate();
+    if (nav !== 0){
+        dt.setMonth(new Date().getMonth() + nav);
+    }
+
+    const day = dt.getDate();
     const month = dt.getMonth();
     const year = dt.getFullYear();
 
-    const firstDayOfMonth = new Date(year, month, 0);
+    const firstDayOfMonth = new Date(year, month, 1);
     const amountOfDaysInMonth = new Date(year, month+1, 0).getDate();
 
     const dateString = firstDayOfMonth.toLocaleDateString('en-us',{
@@ -27,24 +51,106 @@ function load(){
     document.getElementById("currentMonthDisplay").innerText =
         `${dt.toLocaleDateString('en-us',{ month: 'long' })} ${year}`;
 
+
+
+    calendar.innerHTML = '';
+
+    console.log('Days in current month = ' + amountOfDaysInMonth);
+    console.log('last day of previous month = ' + dateString);
+    console.log('Padding days of current month '+ `( ${dt.toLocaleDateString(undefined,{month: 'long'})} )  = `  + paddingDays);
+    console.log('')
+    console.log('')
+    console.log('')
+
     for(let i = 1; i <= paddingDays + amountOfDaysInMonth; i++){
-        const daySquare = document.createElement("div");
+        const daySquare = document.createElement('div');
         daySquare.classList.add('day');
+
+        const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+
 
         if(i > paddingDays){
             daySquare.innerText = i - paddingDays;
+            const eventForDay = events.find(e => e.date === dayString);
 
-            daySquare.addEventListener('click', () => console.log('click'));
+            if (i - paddingDays === day && nav === 0) {
+                daySquare.id = 'currentDay';
+            }
+
+            if (eventForDay){
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
         }else {
             daySquare.classList.add('padding');
         }
 
 
-        document.getElementById("calendar").appendChild(daySquare);
+        calendar.appendChild(daySquare);
+
+        console.log(i - paddingDays + ' ' + day + ' ' + nav)
     }
-    console.log(amountOfDaysInMonth);
-    console.log(dateString);
-    console.log(paddingDays);
+
+
+}
+function closeModal(){
+    eventTitleInput.classList.remove('error');
+    newEventModal.style.display = 'none';
+    deleteEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+
 }
 
+function saveEvent(){
+    if(eventTitleInput.value){
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    }else{
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function deleteEvent(){
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
+}
+
+
+function clickButtons(){
+
+    document.getElementById('nextButton').addEventListener('click', () =>{
+        nav++;
+        load();
+    })
+    document.getElementById('previousButton').addEventListener('click', () =>{
+        nav--;
+        load();
+    })
+
+    document.getElementById('saveBtn').addEventListener('click', saveEvent);
+
+    document.getElementById('cancelBtn').addEventListener('click',closeModal);
+
+    document.getElementById('deleteBtn').addEventListener('click', deleteEvent);
+
+    document.getElementById('closeBtn').addEventListener('click',closeModal);
+}
+
+clickButtons();
 load();
